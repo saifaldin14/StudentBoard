7b64df355834bb19cddb111d511865907d12514c
saifaldin14 <saifaldindeveloper@gmail.com>
2020-10-03 14:29:52 -0400

    Add Input Form

--- src/App.js
+++ src/App.js
@@ -1,41 +1,96 @@
-import React, { Component } from 'react';
-import Scheduler from './components/Scheduler';
-import Toolbar from './components/Toolbar';
-import './App.css';
+import React, { useState } from 'react';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add';
+import Icon from '@material-ui/core/Icon';
 
-const data = [
-  { start_date: '2020-06-10 6:00', end_date: '2020-06-10 8:00', text: 'Event 1', id: 1 },
-  { start_date: '2020-06-13 10:00', end_date: '2020-06-13 18:00', text: 'Event 2', id: 2 }
-];
+import { makeStyles } from '@material-ui/core/styles';
 
-class App extends Component {
-  state = {
-    currentTimeFormatState: true
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+  button: {
+    margin: theme.spacing(1),
+  }
+}))
+
+function App() {
+  const classes = useStyles()
+  const [inputFields, setInputFields] = useState([
+    { firstName: '', lastName: '' },
+    { firstName: '', lastName: '' },
+  ]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("InputFields", inputFields);
   };
 
-  handleTimeFormatStateChange = (state) => {
-    this.setState({
-      currentTimeFormatState: state
-    });
+  const handleChangeInput = (index, event) => {
+    const values = [...inputFields];
+    values[index][event.target.name] = event.target.value;
+    setInputFields(values);
   }
-  render() {
-    const { currentTimeFormatState } = this.state;
-    return (
-      <div>
-        <div className="tool-bar">
-          <Toolbar
-            timeFormatState={currentTimeFormatState}
-            onTimeFormatStateChange={this.handleTimeFormatStateChange}
-          />
-        </div>
-        <div className='scheduler-container'>
-          <Scheduler
-            events={data}
-            timeFormatState={currentTimeFormatState}
-          />
-        </div>
-      </div>
-    );
+
+  const handleAddFields = () => {
+    setInputFields([...inputFields, { firstName: '', lastName: '' }])
   }
+
+  const handleRemoveFields = (index) => {
+    const values = [...inputFields];
+    values.splice(index, 1);
+    setInputFields(values);
+  }
+
+  return (
+    <Container>
+      <h1>Add New Member</h1>
+      <form className={classes.root} onSubmit={handleSubmit}>
+        {inputFields.map((inputField, index) => (
+          <div key={index}>
+            <TextField
+              name="firstName"
+              label="First Name"
+              variant="filled"
+              value={inputField.firstName}
+              onChange={event => handleChangeInput(index, event)}
+            />
+            <TextField
+              name="lastName"
+              label="Last Name"
+              variant="filled"
+              value={inputField.lastName}
+              onChange={event => handleChangeInput(index, event)}
+            />
+            <IconButton
+              onClick={() => handleRemoveFields(index)}
+            >
+              <RemoveIcon />
+            </IconButton>
+            <IconButton
+              onClick={() => handleAddFields()}
+            >
+              <AddIcon />
+            </IconButton>
+          </div>
+        ))}
+        <Button
+          className={classes.button}
+          variant="contained"
+          color="primary"
+          type="submit"
+          endIcon={<Icon>send</Icon>}
+          onClick={handleSubmit}
+        >Send</Button>
+      </form>
+    </Container>
+  );
 }
+
 export default App;
