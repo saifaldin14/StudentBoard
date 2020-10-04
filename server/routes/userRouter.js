d3a6ee2c26779f6d513e3dca6a341f4c166adde6
saifaldin14 <saifaldindeveloper@gmail.com>
2020-10-04 14:00:04 -0400

    Validate login data

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,6 +1,27 @@
 const router = require('express').Router();
+const User = require('../models/useModel');
+router.post("/register", async (req, res) => {
+  try {
+    const { email, password, passwordCheck, displayName } = req.body;
 
-router.get("/test", (req, res) => {
-  res.send("Hello, it's working");
+    //Validate data
+    if (!email || !password || !passwordCheck)
+      return res.status(400).json({ msg: "Not all fields have been entered" });
+
+    if (password.length < 5)
+      return res.status(400).json({ msg: "Password needs to be at least 5 characters long" });
+
+    if (password !== passwordCheck)
+      return res.status(400).json({ msg: "Enter the same password twice for verification" });
+
+    const existingUser = await User.find({ email: email });
+
+    if (existingUser)
+      return res.status(400).json({ msg: "Account with this email already exists" });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 module.exports = router;
