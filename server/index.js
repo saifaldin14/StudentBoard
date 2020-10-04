0d7d4f58bb54490b2f9983adbb55236fb49ad1ae
saifaldin14 <saifaldindeveloper@gmail.com>
2020-10-03 21:27:35 -0400

    Set up express app

--- server/index.js
+++ server/index.js
@@ -1,3 +1,20 @@
+//Required variables
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
+
+//Set up express app
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Set up external port so that when we publish
+//the project the server can connnect with external
+//hosting service
+//If the environment port doesn't exist it will use port 5000
+const PORT = process.env.PORT || 5000;
+//Start up server
+app.listen(PORT, () => {
+  console.log(`The server has started on port ${PORT}`);
+});
