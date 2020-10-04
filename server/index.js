067d873d5facd5c1be730e095f8dfe69c2a45796
saifaldin14 <saifaldindeveloper@gmail.com>
2020-10-04 14:10:08 -0400

    Create user, password hash emplimented

--- server/index.js
+++ server/index.js
@@ -22,7 +22,8 @@ app.listen(PORT, () => {
 //Set up mongoose
 mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true
 }, (err) => {
   if (err) throw err;
   console.log("MongoDB connection established");
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const User = require('../models/useModel');
 router.post("/register", async (req, res) => {
   try {
@@ -14,11 +15,27 @@ router.post("/register", async (req, res) => {
     if (password !== passwordCheck)
       return res.status(400).json({ msg: "Enter the same password twice for verification" });
 
-    const existingUser = await User.find({ email: email });
+    const existingUser = await User.findOne({ email: email });
 
     if (existingUser)
       return res.status(400).json({ msg: "Account with this email already exists" });
 
+    if (!displayName) displayName = email;
+
+    //Apply hashing function to protect password
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    //Create new user
+    const newUser = new User({
+      email,
+      password: passwordHash,
+      displayName
+    });
+
+    //Save user
+    const savedUser = await newUser.save();
+    res.json(savedUser);
   } catch (err) {
     res.status(500).json(err);
   }
